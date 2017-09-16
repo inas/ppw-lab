57de5497d6e4b2ce5f1b97bb667c5bfa1ea63168
anishainas <anishainas@gmail.com>
2017-09-16 20:54:29 +0700

    added test case

--- lab_3/tests.py
+++ lab_3/tests.py
@@ -1,6 +1,9 @@
 from django.test import TestCase, Client
 from django.urls import resolve
 from .views import index
+from .models import Diary
+from django.utils import timezone
+
 
 class Lab3Test(TestCase):
     def test_lab_3_url_is_exist(self):
@@ -14,3 +17,12 @@ class Lab3Test(TestCase):
     def test_lab_3_using_index_func(self):
         found = resolve('/lab-3/')
         self.assertEqual(found.func, index)
+
+    def test_model_can_create_new_activity(self):
+        #Creating a new activity
+        new_activity = Diary.objects.create(date=timezone.now(),activity='Aku mau latihan ngoding deh')
+
+        #Retrieving all available activity
+        counting_all_available_activity = Diary.objects.all().count()
+        self.assertEqual(counting_all_available_activity,1)
+
