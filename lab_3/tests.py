1b6e0c107f70449ff0ce7c9bc309e0740acb5f4f
anishainas <anishainas@gmail.com>
2017-09-16 15:07:47 +0700

    added test to lab_3

--- lab_3/tests.py
+++ lab_3/tests.py
@@ -1,3 +1,16 @@
-from django.test import TestCase
+from django.test import TestCase, Client
+from django.urls import resolve
+from .views import index
 
-# Create your tests here.
+class Lab3Test(TestCase):
+    def test_lab_3_url_is_exist(self):
+        response = Client().get('/lab-3/')
+        self.assertEqual(response.status_code,200)
+
+    def test_lab_3_using_to_do_list_template(self):
+        response = Client().get('/lab-3/')
+        self.assertTemplateUsed(response, 'to_do_list.html')
+
+    def test_lab_3_using_index_func(self):
+        found = resolve('/lab-3/')
+        self.assertEqual(found.func, index)
