56684b1ec55cbbae2e2285538a6d0ae13f3dffaf
anishainas <anishainas@gmail.com>
2017-09-16 20:42:52 +0700

    edited urls

--- lab_3/urls.py
+++ lab_3/urls.py
@@ -1,9 +1,7 @@
 from django.conf.urls import url
 from .views import index
-import lab_3.urls as lab_3
 
 #url for app
 urlpatterns = [
         url(r'^$', index, name='index'),
-        url(r'^lab-3/', include(lab_3,namespace='lab-3')),
 ]
--- praktikum/urls.py
+++ praktikum/urls.py
@@ -18,6 +18,7 @@ from django.contrib import admin
 import lab_1.urls as lab_1
 import lab_2.urls as lab_2
 import lab_2_addon.urls as lab_2_addon
+import lab_3.urls as lab_3
 from lab_1.views import index as index_lab1
 from django.views.generic import RedirectView
 
@@ -26,5 +27,6 @@ urlpatterns = [
     url(r'^lab-1/', include(lab_1,namespace='lab-1')),
     url(r'^lab-2/', include(lab_2,namespace='lab-2')),
     url(r'^lab-2-addon/', include(lab_2_addon,namespace='lab_2_addon')),
+    url(r'^lab-3/', include(lab_3,namespace='lab-3')),
     url(r'^$', RedirectView.as_view(url='lab-2/', permanent=True))
 ]
