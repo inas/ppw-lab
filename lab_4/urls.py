8f8318bea3fc7b2e57de717d49061521dd7abc44
anishainas <anishainas@gmail.com>
2017-09-25 09:03:18 +0700

    pass this stage

--- lab_4/urls.py
+++ lab_4/urls.py
@@ -1,6 +1,7 @@
 from django.conf.urls import url
-from .views import index
+from .views import index, message_post
 
 urlpatterns = [
     url(r'^$', index, name='index'),
+    url(r'^add_message', message_post, name='add_message'),
  ]
--- lab_4/views.py
+++ lab_4/views.py
@@ -1,12 +1,31 @@
 from django.shortcuts import render
 from lab_2.views import landing_page_content
+from django.http import HttpResponseRedirect
+from .forms import Message_Form
+from .models import Message
 
 # Create your views here.
-response = {'author': ""} #TODO Implement yourname
+response = {'author': "Anisha Inas Izdihar"} #TODO Implement yourname
 about_me = 'six words to describe myself hm'
 def index(request):
     response['content'] = landing_page_content
     html = 'lab_4/lab_4.html'
     #TODO Implement, isilah dengan 6 kata yang mendeskripsikan anda
     response['about_me'] = about_me
+    response['message_form'] = Message_Form
     return render(request, html, response)
+
+def message_post(request):
+    form = Message_Form(request.POST or None)
+    if(request.method == 'POST' and form.is_valid()):
+        response['name'] = request.POST['name'] if request.POST['name'] != "" else "Anonymous"
+        response['email'] = request.POST['email'] if request.POST['email'] != "" else "Anonymous"
+        response['message'] = request.POST['message']
+        message = Message(name=response['name'], email=response['email'],
+                          message=response['message'])
+        message.save()
+        html ='lab_4/form_result.html'
+        return render(request, html, response)
+    else:        
+        return HttpResponseRedirect('/lab-4/')
+
