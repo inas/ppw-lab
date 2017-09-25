5ab6d77ffa50d5a7bdd133b4d7275f375e9e8fc2
anishainas <anishainas@gmail.com>
2017-09-25 21:43:12 +0700

    made them prettier

--- lab_2/views.py
+++ lab_2/views.py
@@ -3,7 +3,12 @@ from lab_1.views import mhs_name, birth_date
 
 #TODO Implement
 #Create a content paragraph for your landing page:
-landing_page_content = 'Currently looking forward to learn more things so I can create better landing page. For now, enjoy this very basic page'
+landing_page_content = 'I am a princess cut from marble, smoother than a storm. \
+						And the scars that mark my body, they are silver and gold. \
+						My blood is a flood of rubies, precious stones. \
+						It keeps my veins hot, the fire has found a home in me.\
+						I move through town, I’m quiet like a fight. \
+						And my necklace is of rope, I tie it and untie it'
 
 def index(request):
     response = {'name': mhs_name, 'content': landing_page_content}
