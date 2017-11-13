7a1dc187dcd8b903d3f7a7d028ee1d43b9bbc7f2
anishainas <anishainas@gmail.com>
2017-11-13 10:52:04 +0700

    migrated

--- lab_7/tests.py
+++ lab_7/tests.py
@@ -5,10 +5,10 @@ from django.urls import resolve
 # Create your tests here.
 class Lab6UnitTest(TestCase):
 
-    def test_lab_6_url_is_exist(self):
+    def test_lab_7_url_is_exist(self):
         response = Client().get('/lab-7/')
         self.assertEqual(response.status_code, 200)
 
-    def test_lab6_using_index_func(self):
+    def test_lab7_using_index_func(self):
         found = resolve('/lab-7/')
         self.assertEqual(found.func, index)
