b9b76f548b8030b56b38d5583548deda5f842e5d
anishainas <anishainas@gmail.com>
2017-11-27 18:55:04 +0700

    added test but failed in local

--- lab_8/tests.py
+++ lab_8/tests.py
@@ -1,3 +1,15 @@
 from django.test import TestCase
+from django.test import Client
+from django.urls import resolve
 
 # Create your tests here.
+
+class lab8UnitTest(TestCase):
+    def test_lab_8_url_is_exist(self):
+        response = Client().get('/lab-8/')
+        self.assertEqual(response.status_code, 200)
+
+    def test_lab_8_profile_url_is_exist(self):
+        response = Client().get('/lab-8/profile/')
+        self.assertEqual(response.status_code, 200)
+
